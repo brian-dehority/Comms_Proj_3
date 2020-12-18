d24d026fca7dddd11fcf9bc67301b9b845891ee8
brian-dehority <69400523+brian-dehority@users.noreply.github.com>
2020-12-18 17:17:34 -0500

    Add files via upload

--- correct.m
+++ correct.m
@@ -1,10 +1,16 @@
 function corrected = correct(map, H, rec);
+corrected = zeros(size(rec)); 
 
-corrected = zeros(length(rec));
-for i = 1:length(rec)
+for i = 1:size(rec,1)
     k = rec(i,:); %single received word
+    
     mult = f2mult(k, H.'); %multiply by parity check matrix
-    error = map(bi2de(mult)); %convert to decimal, to find it in the map
-    corrected(i,:) = f2add(k,de2bi(error, length(k))); %correct word
+    multbi = bi2de(mult);
+    if isKey(map,multbi) == 1 %Checks to see if the syndrome is in the map
+        error = map(multbi); %convert to decimal, to find it in the map
+        corrected(i,:) = f2add(k,de2bi(error, length(k))); %correct word
+    else %if syndrome not in map
+        corrected(i,:) = NaN * ones(size(k)); %fill with NaN
+    end
 end 
 end
