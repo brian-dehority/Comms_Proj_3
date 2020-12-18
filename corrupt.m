da0e582c416adb1d5f6cd6023a6689a716a51e41
brian-dehority <69400523+brian-dehority@users.noreply.github.com>
2020-12-18 17:41:28 -0500

    Add files via upload

--- corrupt.m
+++ corrupt.m
@@ -1,6 +1,5 @@
 function corrupted = corrupt(M,p)
-    N = length(M);
-    random = rand(N,1);
+    random = rand(size(M));
     random(random < p) = 0;
     random(random >= p) = 1;
     corrupted = mod(M + random,2);
